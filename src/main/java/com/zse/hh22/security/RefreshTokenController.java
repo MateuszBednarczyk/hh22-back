4e0b2e69eb591030c599b00902d4b6b6e92bcfb9
m.bednarczyk@zsek.olsztyn.pl <3DJMatrix@38>
2022-10-09 14:40:19 +0200

    Refresh token

---
+++ src/main/java/com/zse/hh22/security/RefreshTokenController.java
@@ -0,0 +1,23 @@
+package com.zse.hh22.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user/token/")
+public class RefreshTokenController {
+
+    private final TokenService tokenService;
+
+    @GetMapping("refresh")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        tokenService.refreshToken(request, response);
+    }
+}
