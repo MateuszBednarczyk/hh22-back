339cd41668be20732ec2603c8d2ddf3ab1fede61
m.bednarczyk@zsek.olsztyn.pl <3DJMatrix@38>
2022-10-09 07:01:19 +0200

    init app

---
+++ src/main/java/com/zse/hh22/Hh22Application.java
@@ -0,0 +1,13 @@
+package com.zse.hh22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hh22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hh22Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/zse/hh22/Hh22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zse.hh22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hh22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
