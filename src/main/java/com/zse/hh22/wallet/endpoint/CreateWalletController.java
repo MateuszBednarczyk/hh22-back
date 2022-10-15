dd172fbe0c2f9d694f4434c27c06a82ce6cb21dc
m.bednarczyk@zsek.olsztyn.pl <3DJMatrix@38>
2022-10-15 21:40:06 +0200

    user can create wallet

---
+++ src/main/java/com/zse/hh22/wallet/endpoint/CreateWalletController.java
@@ -0,0 +1,25 @@
+package com.zse.hh22.wallet.endpoint;
+
+import com.zse.hh22.wallet.service.wallet.CreateWalletService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/v1/wallet")
+@RequiredArgsConstructor
+public class CreateWalletController {
+
+    private final CreateWalletService createWalletService;
+
+    @PostMapping("/new")
+    public ResponseEntity<Void> createWallet(Principal loggedUser) {
+        createWalletService.createWallet(loggedUser);
+        return ResponseEntity.ok().build();
+    }
+
+}
