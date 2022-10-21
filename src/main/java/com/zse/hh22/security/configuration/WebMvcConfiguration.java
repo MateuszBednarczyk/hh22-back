8f804471bb0229755415cbb120603e578e8fcf22
m.bednarczyk@zsek.olsztyn.pl <3DJMatrix@38>
2022-10-21 15:31:33 +0200

    deploy

--- src/main/java/com/zse/hh22/security/configuration/WebMvcConfiguration.java
+++ src/main/java/com/zse/hh22/security/configuration/WebMvcConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 import java.io.IOException;
 
 @Configuration
-public class WebMvcCongifuration implements WebMvcConfigurer {
+public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
